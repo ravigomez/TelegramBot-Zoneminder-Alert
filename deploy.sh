6decec4559bb0c71f7a1d742b2dbe449710795b8
Rafael Gómez <ravigomez@gmail.com>
2021-03-15 13:58:05 +0100

    deploy.sh - add correct permision to selenium container

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 git pull
 docker-compose build
 ./scripts/stop.sh 
-./scripts/start.sh 
+./scripts/start.sh
+docker exec -it $(docker ps | grep telegram-bot_selenium-chrome | fmt -w 1 | head -n1) sudo chown -R seluser:seluser /home/seluser/Downloads
