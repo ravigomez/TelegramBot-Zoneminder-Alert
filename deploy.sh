7b08b488a17cc3bfb7c1ee698ad05b51729564ca
Rafael Gómez <ravigomez@gmail.com>
2021-03-15 15:34:36 +0100

    deploy.sh - upgrading deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
+#! /bin/bash
+
 git pull
 docker-compose build
 ./scripts/stop.sh 
 ./scripts/start.sh
-docker exec -it $(docker ps | grep telegram-bot_selenium-chrome | fmt -w 1 | head -n1) sudo chown -R seluser:seluser /home/seluser/Downloads
+
+while :; do
+    CONTAINER_ID=$(docker ps | grep jenkins | fmt -w 1 | head -n1)
+    if [ $CONTAINER_ID ]; then
+        docker exec -it $CONTAINER_ID sudo chown -R seluser:seluser /home/seluser/Downloads
+        break;
+    else
+        sleep 2
+    fi
+done
+
