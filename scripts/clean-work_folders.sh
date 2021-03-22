c07c6ac10b58c7ed8319e3cebbec5fc4592ec59a
Rafael Gómez <ravigomez@gmail.com>
2021-03-22 18:16:33 +0100

    inclide script to clean work dir

---
+++ scripts/clean-work_folders.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+DIRS=("../src/Downloads/" "../src/videos/")
+
+clean(){
+  find $1 -type f -iname $2 -exec rm {} +
+  if [ $? -eq 0 ]; then
+    echo "$1 CLEANED SUCCESSFULLY"
+  fi    
+}
+
+echo "Start to cleaning work folders....."
+clean ${DIRS[0]} "*.avi"
+clean ${DIRS[1]} "*.mp4"
+echo "Finished to cleaning work folders....."
