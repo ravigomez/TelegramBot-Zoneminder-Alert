624f1c3e01db85ea42e974769bb57819020df784
Rafael Gómez <ravigomez@gmail.com>
2021-03-23 12:38:02 +0100

    update clean script

--- scripts/clean-work_folders.sh
+++ scripts/clean-work_folders.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-DIRS=("../src/Downloads/" "../src/videos/")
+DIRS=("../src/videos/")
 
 clean(){
-  find $1 -type f -iname $2 -exec rm {} +
+  find $1 -type f -exec rm -f {} +
   if [ $? -eq 0 ]; then
     echo "$1 CLEANED SUCCESSFULLY"
+  else
+    echo "Erro while tring to clean $1"
   fi    
 }
 
 echo "Start to cleaning work folders....."
-clean ${DIRS[0]} "*.avi"
-clean ${DIRS[1]} "*.mp4"
+clean ${DIRS[0]}
 echo "Finished to cleaning work folders....."
