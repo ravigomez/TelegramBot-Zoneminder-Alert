966d603f53408a6f376cbc3020200cb590b659bf
Rafael Gómez <ravigomez@gmail.com>
2021-03-25 14:30:47 +0100

    update script to run only one command

--- scripts/install_selenium-crontab.sh
+++ scripts/install_selenium-crontab.sh
@@ -4,11 +4,11 @@ echo "Looking for the selenium container..."
 while :; do
     CONTAINER_ID=$(docker ps | grep telegram-bot_selenium-chrome | fmt -w 1 | head -n1)
     if [ $CONTAINER_ID ]; then
-        docker exec -it $CONTAINER_ID sudo apt-get update
-        docker exec -it $CONTAINER_ID sudo apt-get install cron
-        docker exec -it $CONTAINER_ID sudo echo "0 0 * * * find ~/Downloads/ -type f -exec rm -f {} +" >> ~/mycron
-        docker exec -it $CONTAINER_ID sudo crontab -u seluser ~/mycron
-        docker exec -it $CONTAINER_ID sudo rm ~/mycron
+        docker exec -it $CONTAINER_ID sudo apt-get update \
+                                      && sudo apt-get install cron -y \
+                                      && sudo echo "0 0 * * * find ~/Downloads/ -type f -exec rm -f {} +" >> ~/mycron \
+                                      && sudo crontab -u seluser ~/mycron \
+                                      && sudo rm ~/mycron
         echo "selenium container found and crontab configured"
     else
         sleep 2
