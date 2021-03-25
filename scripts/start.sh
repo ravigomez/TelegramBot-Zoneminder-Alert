621fe6eea745baf313fddfa2ae47c3ec707b2e67
Rafael Gómez <ravigomez@gmail.com>
2021-03-25 13:10:07 +0100

    update deploy scripts to retry on error

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,17 @@
-docker stack deploy --compose-file docker-compose.yml telegram-bot
+#! /bin/bash
+
+deploy(){
+    docker stack deploy --compose-file docker-compose.yml telegram-bot
+}
+
+deploy
+
+if [ $? -gt 0]; then
+    sleep 2
+    deploy
+    if [ $? -gt 0]; then
+        echo "Erro while trying to deploy application. Unable to deploy, try again."
+    fi
+fi
+
+echo "Deploy completed successfully!"
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,1 +1,17 @@
-docker stack rm telegram-bot
+#! /bin/bash
+
+undeploy(){
+    docker stack rm telegram-bot
+}
+
+undeploy
+
+if [ $? -gt 0]; then
+    sleep 2
+    undeploy
+    if [ $? -gt 0]; then
+        echo "Erro while trying to undeploy application. Unable to undeploy, try again."
+    fi
+fi
+
+echo "Undeploy completed successfully!"
